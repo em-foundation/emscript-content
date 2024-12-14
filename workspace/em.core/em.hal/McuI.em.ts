31336cb579c42a13c19ccb90c4d6b792a6e79163
bob frankel <bob@biosbob.biz>
2024-12-14 05:47:57 -0800

    em.hal interfaces

---
+++ workspace/em.core/em.hal/McuI.em.ts
@@ -0,0 +1,6 @@
+import em from '@$$em-script'
+export const em$_U = em.declare('INTERFACE')
+
+export interface em$_I {
+    startup(): void
+}
--- workspace/em.core/em.mcu/Common.em.ts
+++ workspace/em.core/em.mcu/Common.em.ts
@@ -1,22 +1,30 @@
 import em from '@$$em-script'
 export const em$_U = em.declare('MODULE')
 
+import * as BusyWaitI from '@em.hal/BusyWaitI.em'
+import * as ConsoleUartI from '@em.hal/ConsoleUartI.em'
 import * as GlobalInterruptsI from '@em.hal/GlobalInterruptsI.em'
-import * as UsCounterI from '@em.hal/UsCounterI.em'
+import * as McuI from '@em.hal/McuI.em'
 
 const em$_C = {
+    BusyWait: new em.proxy<BusyWaitI.em$_I>(),
+    ConsoleUart: new em.proxy<ConsoleUartI.em$_I>(),
     GlobalInterrupts: new em.proxy<GlobalInterruptsI.em$_I>(),
-    UsCounter: new em.proxy<UsCounterI.em$_I>()
+    Mcu: new em.proxy<McuI.em$_I>(),
 }
 
 namespace em$meta {
+    export const x_BusyWait = em$_C.BusyWait
+    export const x_ConsoleUart = em$_C.ConsoleUart
     export const x_GlobalInterrupts = em$_C.GlobalInterrupts
-    export const x_UsCounter = em$_C.UsCounter
+    export const x_Mcu = em$_C.Mcu
 }
 
 namespace em$targ {
+    export const BusyWait = em$_C.BusyWait.unwrap()
+    export const ConsoleUart = em$_C.ConsoleUart.unwrap()
     export const GlobalInterrupts = em$_C.GlobalInterrupts.unwrap()
-    export const UsCounter = em$_C.UsCounter.unwrap()
+    export const Mcu = em$_C.Mcu.unwrap()
 }
 
 export default { em$_U, em$_C, ...em$meta, ...em$targ }
