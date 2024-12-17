cc515f6d071f804d0dc0e7f3cf820ea01203a9bd
bob frankel <bob@biosbob.biz>
2024-12-17 08:47:17 -0800

    em.examples.basic

---
+++ workspace/em.core/em.examples.basic/Ex00_EmptyP.em.ts
@@ -0,0 +1,14 @@
+import em from '@$$em-script'
+export const em$_U = em.declare('MODULE')
+
+const em$_C = {}
+
+namespace em$meta {}
+
+namespace em$targ {
+    export function em$run() {
+        em.halt()
+    }
+}
+
+export default { em$_U, ...em$targ }
