a122116e56030ba57bfe034fc15cc7741868aec4
bob frankel <bob@biosbob.biz>
2024-12-23 04:56:44 -0800

    Prog2: no em$targ

--- workspace/bob.pkg/bob.test/Prog2.em.ts
+++ workspace/bob.pkg/bob.test/Prog2.em.ts
@@ -1,18 +1,14 @@
 import em from '@$$emscript'
 export const em$_U = em.declare('MODULE')
 
-import BusyWait from '@bob.test/BusyWait.em'
-import Pin from '@bob.test/AppLedPin.em'
+import * as BusyWait from '@bob.test/BusyWait.em'
+import * as Pin from '@bob.test/AppLedPin.em'
 
-namespace em$targ {
-    export function em$run() {
-        Pin.makeOutput()
-        for (let i = 0; i < 10; i++) {
-            Pin.toggle()
-            BusyWait.wait(100_000)
-        }
-        Pin.set()
+export function em$run() {
+    Pin.makeOutput()
+    for (let i = 0; i < 10; i++) {
+        Pin.toggle()
+        BusyWait.wait(500_000)
     }
+    Pin.set()
 }
-
-export default { em$_U, ...em$targ }
