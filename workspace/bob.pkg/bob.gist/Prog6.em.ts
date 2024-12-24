864938339f08b2a854c576113cf6e4cb99ccf9e4
bob frankel <bob@biosbob.biz>
2024-12-24 09:52:20 -0800

    Prog6: Common.BusyWait

---
+++ workspace/bob.pkg/bob.gist/Prog6.em.ts
@@ -0,0 +1,21 @@
+import em from '@$$emscript'
+export const em$_U = em.declare('MODULE')
+
+import * as BoardC from '@ti.distro.cc23xx/BoardC.em'
+import * as Common from '@em.mcu/Common.em'
+
+export const AppLedPin = em.delegate(BoardC.AppLedPin)
+export const SysLedPin = em.delegate(BoardC.SysLedPin)
+
+export function em$startup() {
+    AppLedPin.$$.makeOutput()
+    SysLedPin.$$.makeOutput()
+}
+
+export function em$run() {
+    for (let i = 0; i < 10; i++) {
+        AppLedPin.$$.toggle()
+        Common.BusyWait.$$.wait(250_000)
+    }
+    SysLedPin.$$.set()
+}
--- workspace/ti.cc23xx/ti.distro.cc23xx/BoardC.em.ts
+++ workspace/ti.cc23xx/ti.distro.cc23xx/BoardC.em.ts
@@ -1,23 +1,19 @@
 import em from '@$$emscript'
 export const em$_U = em.declare('COMPOSITE')
 
-import * as GpioT from '@ti.mcu.cc23xx/GpioT.em'
-// import * as LedT from '@em.utils/LedT.em'
-
 // import BoardController from '@em.utils/BoardController.em'
-// import BusyWait from '@ti.mcu.cc23xx/BusyWait.em'
-// import Common from '@em.mcu/Common.em'
+import * as BusyWait from '@ti.mcu.cc23xx/BusyWait.em'
+import * as Common from '@em.mcu/Common.em'
 // import ConsoleUart0 from '@ti.mcu.cc23xx/ConsoleUart0.em'
 // import GlobalInterrupts from '@em.arch.arm/GlobalInterrupts.em'
+import * as GpioT from '@ti.mcu.cc23xx/GpioT.em'
+// import * as LedT from '@em.utils/LedT.em'
 // import Mcu from '@ti.mcu.cc23xx/Mcu.em'
 // 
 // export const AppLed = LedT.em$clone()
-// export const AppLedPin = GpioT.em$clone()
+export const AppLedPin = GpioT.em$clone()
 // export const AppOutPin = GpioT.em$clone()
 // export const SysLed = LedT.em$clone()
-// export const SysLedPin = GpioT.em$clone()
-
-export const AppLedPin = GpioT.em$clone()
 export const SysLedPin = GpioT.em$clone()
 
 export function em$configure(): void {
@@ -25,7 +21,7 @@ export function em$configure(): void {
     AppLedPin.pin_num.$bind(15)
     // AppOutPin.pin_num.$bind(20)
     // BoardController.x_Led.$bind(SysLed)
-    // Common.BusyWaitX.$bind(BusyWait)
+    Common.BusyWait.$bind(BusyWait)
     // Common.ConsoleUartX.$bind(ConsoleUart0)
     // Common.GlobalInterruptsX.$bind(GlobalInterrupts)
     // Common.x_Mcu.$bind(Mcu)
