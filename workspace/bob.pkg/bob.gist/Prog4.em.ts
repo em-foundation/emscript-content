20f1dc51401e4c08611c525faecdde74d9dc798b
bob frankel <bob@biosbob.biz>
2024-12-24 05:26:11 -0800

    Prog4: proxy binding

--- workspace/bob.pkg/bob.gist/Prog4.em.ts
+++ workspace/bob.pkg/bob.gist/Prog4.em.ts
@@ -1,15 +1,16 @@
 import em from '@$$emscript'
 export const em$_U = em.declare('MODULE')
 
-import * as BusyWait from '@ti.mcu.cc23xx/BusyWait.em'
-import * as Common from '@em.mcu/Common.em'
+import * as AppLed from '@bob.scratch/AppLed.em'
+import * as AppLedPin from '@bob.scratch/AppLedPin.em'
 
 export namespace em$meta {
     export function em$configure() {
-        Common.BusyWait.$bind(BusyWait)
+        AppLed.Pin.$bind(AppLedPin)
     }
 }
 
 export function em$run() {
+    AppLed.on()
     em.halt()
 }
---
+++ workspace/bob.pkg/bob.scratch/AppLed.em.ts
@@ -0,0 +1,31 @@
+import em from '@$$emscript'
+export const em$_U = em.declare('MODULE')
+
+import * as BusyWait from '@ti.mcu.cc23xx/BusyWait.em'
+import * as GpioI from '@em.hal/GpioI.em'
+
+export const Pin = new em.proxy<GpioI.em$_I>()
+export const active_low = new em.param<em.bool_t>(false)
+
+export function em$startup(): void {
+    Pin.$$.makeOutput()
+    off()
+}
+
+export function off(): void {
+    if (active_low.$$) { Pin.$$.set() } else { Pin.$$.clear() }
+}
+
+export function on(): void {
+    if (active_low.$$) { Pin.$$.clear() } else { Pin.$$.set() }
+}
+
+export function toggle(): void {
+    Pin.$$.toggle()
+}
+
+export function wink(msecs: em.u32): void {
+    on()
+    BusyWait.wait(msecs * 1000)
+    off()
+}
