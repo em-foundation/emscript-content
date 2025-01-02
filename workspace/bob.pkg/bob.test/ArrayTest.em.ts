c4510d6432311fb8ed2eab3c3b8ba60137eee415
bob frankel <bob@biosbob.biz>
2025-01-02 05:10:02 -0800

    em.$reg32

--- workspace/bob.pkg/bob.test/ArrayTest.em.ts
+++ workspace/bob.pkg/bob.test/ArrayTest.em.ts
@@ -9,8 +9,7 @@ export namespace em$meta {
 
 export function em$run() {
     var my_buf = Buf.$make()
-    for (let i = 0; i < my_buf.$len; i++) my_buf[i] = i + 10
-
+    // for (let i = 0; i < my_buf.$len; i++) my_buf[i] = i + 10
     // my_buf[2] = em.$reg32[0xAA]
-    em.$reg32[0xBB] = my_buf[2]
+    em.$reg32[0xBB] = my_buf.$len
 }
--- workspace/bob.pkg/bob.test/RegTest.em.ts
+++ workspace/bob.pkg/bob.test/RegTest.em.ts
@@ -2,6 +2,5 @@ import em from '@$$emscript'
 export const em$_U = em.declare('MODULE')
 
 export function em$run() {
-    em.$reg32[0x1000_0000] = 123
-    em.halt()
+    for (let i = 0; i < 10; i++) em.$reg32[0xaa] = i + 20
 }
