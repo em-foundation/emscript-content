38c25725f0977c323664d26754d686a8306b4e6f
bob frankel <bob@biosbob.biz>
2025-01-07 18:21:19 -0800

    volatile_t<>

--- workspace/ti.cc23xx/ti.mcu.cc23xx/BusyWait.em.ts
+++ workspace/ti.cc23xx/ti.mcu.cc23xx/BusyWait.em.ts
@@ -6,6 +6,6 @@ export const scalar = em.param<em.u8>(3)
 export function wait(usecs: em.u32): void {
     if (usecs == 0) return
     var cnt = usecs * scalar.$$
-    var dummy: em.volatile_t<em.u32>
+    var dummy: volatile_t<u32>
     while (cnt--) dummy = 0
 }
