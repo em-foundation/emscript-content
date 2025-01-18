639ed5ab6cc1b477ee3a456083f94c9d777ed1fb
bob frankel <bob@biosbob.biz>
2025-01-18 15:59:47 -0800

    UsCounter: WIP

--- workspace/em.core/em.hal/UsCounterI.em.ts
+++ workspace/em.core/em.hal/UsCounterI.em.ts
@@ -2,8 +2,6 @@ import em from '@$$emscript'
 export const em$_U = em.declare('INTERFACE')
 
 export interface em$_I {
-    set(time_us: u32): void
-    spin(): void
     start(): void
-    stop(o_raw: em.ptr_t<u32>): u32
+    stop(): u32
 }
---
+++ workspace/ti.cc23xx/ti.mcu.cc23xx/UsCounter.em.ts
@@ -0,0 +1,12 @@
+import em from '@$$emscript'
+export const em$_U = em.declare('MODULE')
+
+import * as em$_R from '@ti.distro.cc23xx/REGS.em'
+
+export function start() {
+
+}
+
+export function stop(): u32 {
+    return 0
+}
