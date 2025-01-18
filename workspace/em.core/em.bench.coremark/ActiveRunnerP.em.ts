c5f3f9d9812eb3861002be63cad0d8083aabf0a9
bob frankel <bob@biosbob.biz>
2025-01-17 17:56:14 -0800

    ActiveRunnerP: working

---
+++ workspace/em.core/em.bench.coremark/ActiveRunnerP.em.ts
@@ -0,0 +1,33 @@
+import em from '@$$emscript'
+export const em$_U = em.declare('MODULE')
+
+import * as BoardC from '@ti.distro.cc23xx/BoardC.em'
+import * as CoreBench from '@em.bench.coremark/CoreBench.em'
+import * as Utils from '@em.bench.coremark/Utils.em'
+
+let ITERATIONS = $param<u16>(10)
+
+export function em$startup() {
+    CoreBench.setup()
+}
+
+export function em$run() {
+    // AppLed.on()
+    // Common.BusyWait.wait(250000)
+    // AppLed.off()
+    // Common.UsCounter.start()
+    em.$['%%d+']
+    for (let i = 0; i < ITERATIONS.$$; i++) {
+        CoreBench.run(0)
+    }
+    em.$['%%d-']
+    // auto usecs = Common.UsCounter.stop()
+    // AppLed.on()
+    // Common.BusyWait.wait(250000)
+    // AppLed.off()
+    // printf "usecs = %d\n", usecs
+    printf`list crc = %04x\n`(Utils.getCrc(Utils.Kind.LIST))
+    printf`matrix crc = %04x\n`(Utils.getCrc(Utils.Kind.MATRIX))
+    printf`state crc = %04x\n`(Utils.getCrc(Utils.Kind.STATE))
+    printf`final crc = %04x\n`(Utils.getCrc(Utils.Kind.FINAL))
+}
--- workspace/em.core/em.bench.coremark/CoreBench.em.ts
+++ workspace/em.core/em.bench.coremark/CoreBench.em.ts
@@ -1,6 +1,7 @@
 import em from '@$$emscript'
 export const em$_U = em.declare('MODULE')
 
+import * as Crc from '@em.bench.coremark/Crc.em'
 import * as ListBench from '@em.bench.coremark/ListBench.em'
 import * as MatrixBench from '@em.bench.coremark/MatrixBench.em'
 import * as StateBench from '@em.bench.coremark/StateBench.em'
@@ -25,35 +26,54 @@ export namespace em$meta {
     }
 }
 
+export function kind(): Utils.Kind {
+    return Utils.Kind.FINAL
+}
+
 export function print() {
     ListBench.print()
     MatrixBench.print()
     StateBench.print()
 }
 
-export function em$run() {
-
-    em.$['%%a']
-    ListBench.setup()
-    em.$['%%a+']
+export function run(arg: i16): Utils.sum_t {
     let crc = ListBench.run(1)
-    em.$['%%a-']
-    ListBench.print()
-    printf`\ncrc = %04x\n`(crc)
-
-    // em.$['%%a']
-    // StateBench.setup()
-    // em.$['%%a+']
-    // let crc = StateBench.run(0)
-    // em.$['%%a-']
-    // StateBench.print()
-    // printf`\ncrc = %04x\n`(crc)
-
-    // em.$['%%a']
-    // MatrixBench.setup()
-    // em.$['%%a+']
-    // let crc = MatrixBench.run(0)
-    // em.$['%%a-']
-    // MatrixBench.print()
-    // printf`\ncrc = %04x\n`(crc)
+    Utils.setCrc(Utils.Kind.FINAL, Crc.add16(<i16>crc, Utils.getCrc(Utils.Kind.FINAL)))
+    crc = ListBench.run(-1)
+    Utils.setCrc(Utils.Kind.FINAL, Crc.add16(<i16>crc, Utils.getCrc(Utils.Kind.FINAL)))
+    Utils.bindCrc(Utils.Kind.LIST, Utils.getCrc(Utils.Kind.FINAL))
+    return Utils.getCrc(Utils.Kind.FINAL)
 }
+
+export function setup() {
+    ListBench.setup()
+    MatrixBench.setup()
+    StateBench.setup()
+}
+
+// export function em$run() {
+// 
+//     em.$['%%a']
+//     ListBench.setup()
+//     em.$['%%a+']
+//     let crc = ListBench.run(1)
+//     em.$['%%a-']
+//     ListBench.print()
+//     printf`\ncrc = %04x\n`(crc)
+// 
+//     // em.$['%%a']
+//     // StateBench.setup()
+//     // em.$['%%a+']
+//     // let crc = StateBench.run(0)
+//     // em.$['%%a-']
+//     // StateBench.print()
+//     // printf`\ncrc = %04x\n`(crc)
+// 
+//     // em.$['%%a']
+//     // MatrixBench.setup()
+//     // em.$['%%a+']
+//     // let crc = MatrixBench.run(0)
+//     // em.$['%%a-']
+//     // MatrixBench.print()
+//     // printf`\ncrc = %04x\n`(crc)
+// }
