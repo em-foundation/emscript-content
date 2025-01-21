6a4e849af093774581166cf79d46d966a505c153
bob frankel <bob@biosbob.biz>
2025-01-20 21:27:30 -0800

    IntrVec: simplify

--- workspace/em.core/em.arch.arm/IntrVec.em.ts
+++ workspace/em.core/em.arch.arm/IntrVec.em.ts
@@ -24,6 +24,7 @@ export namespace em$meta {
 
     const intr_list = new Array<string>()
     const used_list = new Array<string>()
+    const used_set = new Set<string>()
 
     export function em$init() {
         em$_U.used()
@@ -32,25 +33,6 @@ export namespace em$meta {
 
     export function em$generate() {
         let out = $outfile('em.arch.arm/intr.cpp')
-        for (let n of intr_list) {
-            if (n == NO_VEC) continue
-            out.addFrag(`
-                        |-> #define __${n}_isr _DEFAULT_isr
-            `)
-        }
-        out.addFrag(`
-                        |-> //
-                        |-> extern "C" void DEFAULT_isr$$();
-                        |-> void _DEFAULT_isr( void ) { DEFAULT_isr$$(); }
-                        |-> // used 
-        `)
-        for (let n of used_list) {
-            out.addFrag(`
-                        |-> #undef __${n}_isr
-                        |-> #define __${n}_isr ${n}_isr$$
-                        |-> extern "C" void ${n}_isr$$(); // __attribute__((weak, alias("_Z12_DEFAULT_isrv")));
-            `)
-        }
         out.addFrag(`
                         |-> //
                         |-> #include <stdbool.h>
@@ -62,14 +44,14 @@ export namespace em$meta {
                         |-> extern uint32_t __stack_top__;
                         |-> extern "C" void em__start( void );
                         |-> 
-                        |-> extern void DEFAULT_isr( void );
+                        |-> extern "C" void DEFAULT_isr$$( void );
                         |-> 
                         |-> extern "C" const intvec_elem  __attribute__((section(".intvec"))) __vector_table[35] = {
                         |->     { .ptr = (void*)&__stack_top__ },
                         |->     { .fxn = em__start },
         `)
         for (let n of intr_list) {
-            const s = n == NO_VEC ? '0' : `__${n}_isr`
+            const s = n == NO_VEC ? '0' : used_set.has(n) ? `${n}_isr$$` : 'DEFAULT_isr$$'
             out.addFrag(`
                         |-> /**/${s},
             `)
@@ -86,6 +68,7 @@ export namespace em$meta {
 
     export function useIntr(name: string) {
         used_list.push(name)
+        used_set.add(name)
     }
 }
 
