0368c1d88b68a3eb3a3b38cf46c75a5d11e9dd2c
bob frankel <bob@biosbob.biz>
2025-01-22 15:39:41 -0800

    Ex13_TickerP: working

---
+++ workspace/em.core/em.examples.basic/Ex13_TickerP.em.ts
@@ -0,0 +1,36 @@
+import em from '@$$emscript'
+export const em$_U = em.$declare('MODULE')
+
+import * as BoardC from '@ti.distro.cc23xx/BoardC.em'
+import * as FiberMgr from '@em.utils/FiberMgr.em'
+import * as TickerMgr from '@em.utils/TickerMgr.em'
+import * as TimeTypes from '@em.utils/TimeTypes.em'
+
+export const AppLed = $delegate(BoardC.AppLed)
+export const SysLed = $delegate(BoardC.SysLed)
+
+let app_ticker = $param<TickerMgr.Obj>()
+let sys_ticker = $param<TickerMgr.Obj>()
+
+export namespace em$meta {
+    export function em$construct() {
+        app_ticker.$$ = TickerMgr.em$meta.create()
+        sys_ticker.$$ = TickerMgr.em$meta.create()
+    }
+}
+
+export function em$run() {
+    app_ticker.$$.$$.start(TimeTypes.Secs24p8_initMsecs(1_000), $cb(appTickCb))
+    sys_ticker.$$.$$.start(TimeTypes.Secs24p8_initMsecs(1_500), $cb(sysTickCb))
+    FiberMgr.run()
+}
+
+function appTickCb() {
+    em.$['%%c']
+    AppLed.$$.wink(100)
+}
+
+function sysTickCb() {
+    em.$['%%d']
+    SysLed.$$.wink(100)
+}
