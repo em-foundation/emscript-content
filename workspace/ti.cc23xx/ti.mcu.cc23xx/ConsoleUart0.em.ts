fad7eed532876fadc11fbfe724f33bcf3b3b82e6
bob frankel <bob@biosbob.biz>
2025-01-23 05:38:14 -0800

    SleepyRunnerP: working

--- workspace/ti.cc23xx/ti.mcu.cc23xx/ConsoleUart0.em.ts
+++ workspace/ti.cc23xx/ti.mcu.cc23xx/ConsoleUart0.em.ts
@@ -3,8 +3,17 @@ export const em$_U = em.$declare('MODULE')
 
 import * as $R from '@ti.distro.cc23xx/REGS.em'
 import * as GpioI from '@em.hal/GpioI.em'
+import * as Idle from '@ti.mcu.cc23xx/Idle.em'
 
 export const TxPin = $proxy<GpioI.em$_I>()
+
+export namespace em$meta {
+
+    export function em$configure() {
+        Idle.em$meta.addSleepEnter($cb(sleepEnter))
+        Idle.em$meta.addSleepLeave($cb(sleepLeave))
+    }
+}
 
 export function em$startup(): void {
     $R.CLKCTL.CLKENSET0.$$ = $R.CLKCTL_CLKENSET0_UART0
@@ -25,4 +34,21 @@ export function flush(): void {
 export function put(data: u8): void {
     $R.UART0.DR.$$ = data
     flush()
+}
+
+function sleepEnter() {
+    $R.CLKCTL.CLKENCLR0.$$ = $R.CLKCTL_CLKENCLR0_UART0
+    TxPin.$$.reset()
+}
+
+function sleepLeave() {
+    $R.CLKCTL.CLKENSET0.$$ = $R.CLKCTL_CLKENSET0_UART0
+    TxPin.$$.makeOutput()
+    TxPin.$$.set()
+    TxPin.$$.functionSelect(2)
+    $R.UART0.CTL.$$ &= ~$R.UART_CTL_UARTEN
+    $R.UART0.IBRD.$$ = 26
+    $R.UART0.FBRD.$$ = 3
+    $R.UART0.LCRH.$$ = $R.UART_LCRH_WLEN_BITL8
+    $R.UART0.CTL.$$ |= $R.UART_CTL_UARTEN
 }
