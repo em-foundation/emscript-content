e1b1c56f0f5067bcb0a5da2ce2ce540fe3ab116f
bob frankel <bob@biosbob.biz>
2025-01-27 10:56:57 -0800

    WIP

--- workspace/bob.pkg/bob.test/SemTok.em.ts
+++ workspace/bob.pkg/bob.test/SemTok.em.ts
@@ -1,18 +1,4 @@
 import em from '@$$emscript'
 export const $U = em.$declare('MODULE')
 
-import * as Common from '@em.mcu/Common.em'
-import * as BusyWait from '@ti.mcu.cc23xx/BusyWait.em'
-
-Common.BusyWait.$$.wait(100)
-BusyWait.wait(200)
-
-type MyInt = u8
-type MyInt2 = MyInt
-
-const x = <MyInt2>10
-
-function foo(x: MyInt2) {
-    let y = 10
-    x += y
-}
+import * as GpioI from '@em.hal/GpioI.em'
