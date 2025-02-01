ae63330842772a21135a4f55f6135549780ece83
Geoffrey Cohler <g.cohler@computer.org>
2025-02-01 10:05:44 -0500

    refactor combo demo (#2)

--- workspace/em.core/em.examples.combo/Ex01_TickerP.em.ts
+++ workspace/em.core/em.examples.combo/Ex01_TickerP.em.ts
@@ -7,21 +7,28 @@ import * as FiberMgr from '@em.utils/FiberMgr.em'
 import * as TickerMgr from '@em.utils/TickerMgr.em'
 import * as TimeTypes from '@em.utils/TimeTypes.em'
 
-export const AppLed = $delegate(BoardC.AppLed)
-export const AppBut = $delegate(BoardC.AppBut)
-export const SysLed = $delegate(BoardC.SysLed)
-
-let app_ticker = $config<TickerMgr.Obj>()
-let sys_ticker = $config<TickerMgr.Obj>()
-let print_ticker = $config<TickerMgr.Obj>()
-
-const max_sys_led_ticks = TimeTypes.Secs24p8_initMsecs(1_500) // 1.5s
-const max_app_led_ticks = TimeTypes.Secs24p8_initMsecs(2_000) // 2s
-const print_ticks = TimeTypes.Secs24p8_initMsecs(5_000) // 5s
+// app settings
+const sysLedPeriodMs = 1500
+const appLedPeriodMs = 2000
+const printPeriodMs = 5000
 const min_press_time = 10 // 10ms
 const max_press_time = 2000 // 2s
-let divided_by: u32 = 1
+const maxDividedBy = 8
 
+const AppLed = $delegate(BoardC.AppLed)
+const AppBut = $delegate(BoardC.AppBut)
+const SysLed = $delegate(BoardC.SysLed)
+const app_ticker = $config<TickerMgr.Obj>()
+const sys_ticker = $config<TickerMgr.Obj>()
+const print_ticker = $config<TickerMgr.Obj>()
+
+const max_sys_led_ticks = TimeTypes.Secs24p8_initMsecs(sysLedPeriodMs)
+const max_app_led_ticks = TimeTypes.Secs24p8_initMsecs(appLedPeriodMs)
+const print_ticks = TimeTypes.Secs24p8_initMsecs(printPeriodMs)
+const ticksPerSec = TimeTypes.Secs24p8_initMsecs(TimeTypes.millisecondsPerSecond)
+
+// initial state vector
+let divided_by: u32 = 1
 let sys_count: u32 = 0
 let app_count: u32 = 0
 let last_app_count: u32 = 0
@@ -63,7 +70,7 @@ function onButtonPressed() {
         last_sys_count = 0;
     } else {
         // a short press (min_press_time < press time < max_press_time)
-        divided_by = (divided_by >= 8 || divided_by < 1) ? 1 : divided_by * 2
+        divided_by = (divided_by >= maxDividedBy || divided_by < 1) ? 1 : divided_by * 2
         printf`Short button press: Setting rate to %dx\n`(divided_by)
         printStatus();
         app_ticker.$$.$$.start(max_app_led_ticks / divided_by, $cb(appTickCb))
@@ -72,11 +79,20 @@ function onButtonPressed() {
 }
 
 function printStatus() {
-    printf`Button effects:\n... short press (>%d ms): cycle through rates (1,2,4,8x)\n... long press (>%d s): stop led tickers\n`(min_press_time, max_press_time/1000)
+    printf`Button effects:\n... short press (>%d ms): cycle through rates (1,2,4,8x)\n... long press (>%d s): stop led tickers\n`(
+        min_press_time,
+        max_press_time/TimeTypes.millisecondsPerSecond
+    )
     printf`Current rate %dx\n`(divided_by)
-    printf`... should print every ~%ds\n`(print_ticks / 256)
-    printf`... app ticks should be %d..%d\n`(divided_by * print_ticks / max_app_led_ticks, (divided_by * print_ticks / max_app_led_ticks) + 1)
-    printf`... sys ticks should be %d..%d\n`(divided_by * print_ticks / max_sys_led_ticks, (divided_by * print_ticks / max_sys_led_ticks) + 1)
+    printf`... should print every ~%ds\n`(print_ticks / ticksPerSec)
+    printf`... app ticks should be %d..%d\n`(
+        divided_by * print_ticks / max_app_led_ticks,
+        (divided_by * print_ticks / max_app_led_ticks) + 1
+    )
+    printf`... sys ticks should be %d..%d\n`(
+        divided_by * print_ticks / max_sys_led_ticks,
+        (divided_by * print_ticks / max_sys_led_ticks) + 1
+    )
 }
 
 function printTickCb() {
@@ -85,10 +101,20 @@ function printTickCb() {
     const delta_sys_count = sys_count - last_sys_count
     const min_delta_app_count = divided_by * print_ticks / max_app_led_ticks
     const min_delta_sys_count = divided_by * print_ticks / max_sys_led_ticks
-    const delta_app_err = (delta_app_count < min_delta_app_count || delta_app_count > min_delta_app_count + 1) ? t$`*` : t$``
-    const delta_sys_err = (delta_sys_count < min_delta_sys_count || delta_sys_count > min_delta_sys_count + 1) ? t$`*` : t$``
-    printTime()
-    printf`:  Hello World:  rate: %dx  ticks(app,sys): (%d%s,%d%s)\n`(divided_by, delta_app_count, delta_app_err, delta_sys_count, delta_sys_err)
+    const delta_app_err = (delta_app_count < min_delta_app_count || delta_app_count > min_delta_app_count + 1) 
+        ? t$`*`
+        : t$``
+    const delta_sys_err = (delta_sys_count < min_delta_sys_count || delta_sys_count > min_delta_sys_count + 1) 
+        ? t$`*`
+        : t$``
+    printTime(Common.Uptimer.$$.read())
+    printf`:  Hello World:  rate: %dx  ticks(app,sys): (%d%s,%d%s)\n`(
+        divided_by,
+        delta_app_count,
+        delta_app_err,
+        delta_sys_count,
+        delta_sys_err
+    )
     if (divided_by > 0 && last_sys_count > 0 && last_sys_count == sys_count) {
         printf`No sys ticks detected since last print\n`()
         em.halt()
@@ -101,15 +127,15 @@ function printTickCb() {
     last_sys_count = sys_count
 }
 
-function printTime() {
-    const raw_time = Common.Uptimer.$$.read()
-    const raw_secs = raw_time.secs
-    const raw_msecs = TimeTypes.RawSubsToMsecs(raw_time.subs)
-    const days = raw_secs / (24 * 3600)
-    const hours = (raw_secs % (24 * 3600)) / 3600
-    const minutes = (raw_secs % 3600) / 60
-    const seconds = raw_secs % 60
-    printf`%dT%02d:%02d:%02d.%03d`(days, hours, minutes, seconds, raw_msecs)
+function printTime(rawTime: TimeTypes.RawTime) {
+    const timeParts = TimeTypes.RawTimeToTimeParts(rawTime)
+    printf`%dT%02d:%02d:%02d.%03d`(
+        timeParts.days,
+        timeParts.hours,
+        timeParts.minutes,
+        timeParts.seconds,
+        timeParts.milliseconds
+    )
 }
 
 function sysTickCb() {
--- workspace/em.core/em.utils/TimeTypes.em.ts
+++ workspace/em.core/em.utils/TimeTypes.em.ts
@@ -1,9 +1,24 @@
 import em from '@$$emscript'
 export const $U = em.$declare('MODULE')
 
+export const hoursPerDay = 24
+export const minutesPerHour = 60
+export const secondsPerMinute = 60
+export const secondsPerHour = minutesPerHour * secondsPerMinute
+export const secondsPerDay = hoursPerDay * secondsPerHour
+export const millisecondsPerSecond = 1000
+
 export class RawTime extends $struct {
     secs: u32
     subs: u32
+}
+
+export class TimeParts extends $struct {
+    days: u32
+    hours: u32
+    minutes: u32
+    seconds: u32
+    milliseconds: u32
 }
 
 export type Secs24p8 = u32
@@ -18,4 +33,14 @@ export function Secs24p8_initMsecs(msecs: u32): Secs24p8 {
 
 export function Secs24p8_ZERO(): Secs24p8 {
     return 0
+}
+
+export function RawTimeToTimeParts(rawTime: RawTime): TimeParts {
+    let timeParts = TimeParts.$make()
+    timeParts.days = rawTime.secs / secondsPerDay
+    timeParts.hours = (rawTime.secs % secondsPerDay) / secondsPerHour
+    timeParts.minutes = (rawTime.secs % secondsPerHour) / secondsPerMinute
+    timeParts.seconds = rawTime.secs % secondsPerMinute
+    timeParts.milliseconds = RawSubsToMsecs(rawTime.subs)
+    return timeParts
 }
