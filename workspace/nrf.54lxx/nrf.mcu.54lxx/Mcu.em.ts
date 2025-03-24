08fade8893baf71c358a0924b7ef9c4fb2c3b005
bob frankel <bob@biosbob.biz>
2025-03-24 06:14:49 -0700

    nrf: setup-sram support

--- workspace/nrf.54lxx/nrf.mcu.54lxx/Mcu.em.ts
+++ workspace/nrf.54lxx/nrf.mcu.54lxx/Mcu.em.ts
@@ -4,11 +4,19 @@ export const $U = em.$declare('MODULE', McuI)
 import * as Debug from '@em.lang/Debug.em'
 import * as McuI from '@em.hal/McuI.em'
 
-export namespace em$meta { }
+const use_sram = $config<bool_t>()
+
+export namespace em$meta {
+    export function em$construct() {
+        use_sram.$$ = $property('em.build.BootFlash', false)
+    }
+}
 
 export function startup(): void {
     e$`NRF_OSCILLATORS_S->PLL.FREQ = 1` // 128 MHz
-    e$`NRF_APPLICATION_ICACHE_S->ENABLE = 1`
+    if (!use_sram.$$) {
+        e$`NRF_APPLICATION_ICACHE_S->ENABLE = 1`
+    }
     Debug.startup()
     $['%%a:'](2)
 }
