d6fd6db04016048338880b9e7d8da4fb69e48c7e
bob frankel <bob@biosbob.biz>
2025-03-24 07:49:24 -0700

    nrf: interrupts

--- workspace/nrf.54lxx/nrf.distro.54lxx/BuildC.em.ts
+++ workspace/nrf.54lxx/nrf.distro.54lxx/BuildC.em.ts
@@ -13,6 +13,276 @@ import * as StartupC from '@nrf.distro.54lxx/StartupC.em'
 import * as TargC from '@em.lang/TargC.em'
 
 const NVIC_INTRS = <Array<string>>[
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    'SWI00',
+    'SWI01',
+    'SWI02',
+    'SWI03',
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    'SPU00',
+    'MPC00',
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    'AAR00_CCM00',
+    'ECB00',
+    'CRACEN',
+    null,                           /*Reserved */
+    'SERIAL00',
+    'RRAMC',
+    'VPR00',
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    'CTRLAP',
+    null,                           /*Reserved */
+    'CM33SS',
+    'TIMER00',
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    'SPU10',
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    'TIMER10',
+    null,                           /*Reserved */
+    'EGU10',
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    'RADIO_0',
+    'RADIO_1',
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    'SPU20',
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    'SERIAL20',
+    'SERIAL21',
+    'SERIAL22',
+    'EGU20',
+    'TIMER20',
+    'TIMER21',
+    'TIMER22',
+    'TIMER23',
+    'TIMER24',
+    null,                         /*Reserved */
+    'PDM20',
+    'PDM21',
+    'PWM20',
+    'PWM21',
+    'PWM22',
+    'SAADC',
+    'NFCT',
+    'TEMP',
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    'GPIOTE20_0',
+    'GPIOTE20_1',
+    'TAMPC',
+    'I2S20',
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    'QDEC20',
+    'QDEC21',
+    'GRTC_0',
+    'GRTC_1',
+    'GRTC_2',
+    'GRTC_3',
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    'SPU30',
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    'SERIAL30',
+    'CLOCK_POWER',
+    'COMP_LPCOMP',
+    null,                           /*Reserved */
+    'WDT30',
+    'WDT31',
+    null,                           /*Reserved */
+    null,                           /*Reserved */
+    'GPIOTE30_0',
+    'GPIOTE30_1',
 ]
 
 export function em$configure() {
