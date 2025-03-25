148ef9ac8641544b8e5f9ecd55d1b3f7024e193a
bob frankel <bob@biosbob.biz>
2025-03-24 18:05:27 -0700

    cleanup

--- workspace/nrf.54lxx/nrf.mcu.54lxx/UptimerN.em.ts
+++
@@ -1,9 +0,0 @@
-import em from '@$$emscript'
-export const $U = em.$declare('MODULE', UptimerI)
-
-import * as TimeTypes from '@em.utils/TimeTypes.em'
-import * as UptimerI from '@em.hal/UptimerI.em'
-
-export function read(): TimeTypes.RawTime {
-    return TimeTypes.RawTime_ZERO()
-}
