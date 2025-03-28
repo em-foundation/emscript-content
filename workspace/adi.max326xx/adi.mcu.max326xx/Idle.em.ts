8b032273cfa483fa7de96ee028c02893e9116edf
bob frankel <bob@biosbob.biz>
2025-03-27 19:36:06 -0700

    adi: Idle WIP

--- workspace/adi.max326xx/adi.mcu.max326xx/Idle.em.ts
+++ workspace/adi.max326xx/adi.mcu.max326xx/Idle.em.ts
@@ -10,7 +10,7 @@ export namespace em$meta { }
 
 //>> ---- em$targ ---- <<//
 
-var cur_pause_only = true
+var cur_pause_only = false
 
 export function em$startup() {
     $['%%b+']
@@ -29,8 +29,11 @@ function doSleep() {
     $['%%b:'](2)
     $['%%b-']
     IntrVec.PRIMASK_set(1)
-    // $R.PWRSEQ.LPCN.$$ |= $R.F_PWRSEQ_LPCN_LPWKST_CLR
     $R.MCR.CTRL.$$ |= $R.F_MCR_CTRL_ERTCO_EN
+
+    // !!!! BEWARE OF THE FOLLOWING LINE !!!!
+    // $R.PWRSEQ.LPCN.$$ |= $R.F_PWRSEQ_LPCN_LPWKST_CLR
+
     e$`SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk`
     // $R.GCR.PM.$$ |= $R.S_GCR_PM_MODE_STANDBY
     e$`asm volatile ("wfi")`
@@ -47,8 +50,7 @@ export function exec() {
 }
 
 export function setPauseOnly(pause_only: bool_t) {
-    // cur_pause_only = pause_only
-    cur_pause_only = true
+    cur_pause_only = pause_only
 }
 
 export function wakeup() { }
