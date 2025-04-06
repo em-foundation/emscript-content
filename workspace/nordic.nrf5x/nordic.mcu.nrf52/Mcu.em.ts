607e14a0bd93e882c49368618475108aed3b6eda
bob frankel <bob@biosbob.biz>
2025-04-06 11:09:57 -0700

    nrf52: WIP

--- workspace/nordic.nrf5x/nordic.mcu.nrf52/Mcu.em.ts
+++ workspace/nordic.nrf5x/nordic.mcu.nrf52/Mcu.em.ts
@@ -71,6 +71,10 @@ export function startup(): void {
     if (!use_sram.$$) {
         $R.NVMC.ICACHECNF.$$ = 1
     }
+    // $R.POWER.RAM[2].POWER.$$ = 0
+    // $R.POWER.RAM[3].POWER.$$ = 0
+    // $R.POWER.RAM[6].POWER.$$ = 0
+    // $R.POWER.RAM[7].POWER.$$ = 0
     $R.POWER.DCDCEN.$$ = 1
     $R.CLOCK.LFCLKSRC.$$ = $R.CLOCK_LFCLKSRCCOPY_SRC_Xtal
     $R.CLOCK.TASKS_LFCLKSTART.$$ = 1
