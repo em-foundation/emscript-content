111841f86d5196c147f351549b400ce9819d5f27
bob frankel <bob@biosbob.biz>
2025-04-10 20:35:44 -0700

    ti.RfFreq

--- workspace/ti.cc23xx/ti.radio.cc23xx/RadioDriver.em.ts
+++ workspace/ti.cc23xx/ti.radio.cc23xx/RadioDriver.em.ts
@@ -25,6 +25,7 @@ enum State {
 export namespace em$meta {
     export function em$construct() {
         IntrVec.em$meta.useIntr('LRFD_IRQ0')
+        $U.auxH()
     }
 }
 
@@ -49,11 +50,95 @@ export function enable() {
     em.$reg32[$R.LRFDRFE_BASE + $R.LRFDRFE_O_RSSI] = 127
     em.$reg16[$R.LRFD_BUFRAM_BASE + $R.PBE_COMMON_RAM_O_FIFOCMDADD] = <u16>(($R.LRFDPBE_BASE + $R.LRFDPBE_O_FCMD) & 0x0FFF) >> 2
     RfTrim.apply()
+    switch (RadioConfig.getPhy()) {
+        case RadioConfig.Phy.BLE_1M:
+            em.$reg32[$R.LRFDPBE32_BASE + $R.LRFDPBE32_O_MDMSYNCA] = 0x8E89_BED6
+            em.$reg32[$R.LRFD_BUFRAM_BASE + $R.PBE_BLE5_RAM_O_CRCINITL] = (0x555555 << 8)
+            em.$reg16[$R.LRFD_BUFRAM_BASE + $R.PBE_BLE5_RAM_O_EXTRABYTES] = 6 // stat + rssi + timestamp
+            em.$reg16[$R.LRFD_BUFRAM_BASE + $R.PBE_BLE5_RAM_O_OWNADRL] = 0xAAAA
+            em.$reg16[$R.LRFD_BUFRAM_BASE + $R.PBE_BLE5_RAM_O_OWNADRM] = 0xBBBB
+            em.$reg16[$R.LRFD_BUFRAM_BASE + $R.PBE_BLE5_RAM_O_OWNADRH] = 0xCCCC
+            em.$reg16[$R.LRFD_BUFRAM_BASE + $R.PBE_BLE5_RAM_O_ADVCFG] = 0
+            em.$reg16[$R.LRFD_BUFRAM_BASE + $R.PBE_BLE5_RAM_O_FILTPOLICY] = 0
+            em.$reg16[$R.LRFD_BUFRAM_BASE + $R.PBE_BLE5_RAM_O_RPACONNECT] = 0
+            em.$reg16[$R.LRFD_BUFRAM_BASE + $R.PBE_BLE5_RAM_O_RPACONNECT] = 0
+            em.$reg16[$R.LRFD_BUFRAM_BASE + $R.PBE_BLE5_RAM_O_FL1MASK] = 0
+            em.$reg16[$R.LRFD_BUFRAM_BASE + $R.PBE_BLE5_RAM_O_FL2MASK] = 0
+            em.$reg16[$R.LRFD_BUFRAM_BASE + $R.PBE_BLE5_RAM_O_OPCFG] = 0
+            break
+        case RadioConfig.Phy.PROP_1M:
+        case RadioConfig.Phy.PROP_250K:
+            em.$reg32[$R.LRFDPBE32_BASE + $R.LRFDPBE32_O_MDMSYNCA] = 0x7B8A_D0C9 // scramble(0x930B_51DE)
+            break
+    }
+    setState(State.READY)
 }
 
+function freqFromChan(chan: u32): u32 {
+    const BASE = 2_404_000_000
+    const SPACE = 2_000_000
+    return (chan >= 0 && chan <= 10) ? BASE + (chan * SPACE) :
+        (chan >= 11 && chan <= 36) ? BASE + (chan * SPACE) + SPACE :
+            (chan == 37) ? 2_402_000_000 :
+                (chan == 38) ? 2_426_000_000 :
+                    (chan == 39) ? 2_480_000_000 :
+                        0
+}
+
+
 function setState(s: State) {
-    // em.@"%%[a:]"(@intFromEnum(s));
+    // em.@"%%[a:]"(@intFromEnum(s))
     cur_state = s
 }
 
+export function startCs(chan: u8, timeout: u16) {
+    setState(State.CS)
+    RfCtrl.enableImages()
+    const cfg_val: u32 =
+        (0 << $R.PBE_GENERIC_RAM_OPCFG_RXFILTEROP_S) |
+        (1 << $R.PBE_GENERIC_RAM_OPCFG_RXINCLUDEHDR_S) |
+        (1 << $R.PBE_GENERIC_RAM_OPCFG_RXREPEATNOK_S) |
+        (0 << $R.PBE_GENERIC_RAM_OPCFG_START_S) |
+        // (1 << $R.PBE_GENERIC_RAM_OPCFG_FS_NOCAL_S) |
+        // (1 << $R.PBE_GENERIC_RAM_OPCFG_FS_KEEPON_S) |
+        (1 << $R.PBE_GENERIC_RAM_OPCFG_NEXTOP_S) |
+        (1 << $R.PBE_GENERIC_RAM_OPCFG_SINGLE_S) |
+        (0 << $R.PBE_GENERIC_RAM_OPCFG_IFSPERIOD_S) |
+        (0 << $R.PBE_GENERIC_RAM_OPCFG_RXREPEATOK_S) |
+        (0 << $R.PBE_GENERIC_RAM_OPCFG_RFINTERVAL_S)
+    em.$reg16[$R.LRFD_BUFRAM_BASE + $R.PBE_GENERIC_RAM_O_OPCFG] = <u16>cfg_val
+    em.$reg16[$R.LRFD_BUFRAM_BASE + $R.PBE_GENERIC_RAM_O_NESB] = $R.PBE_GENERIC_RAM_NESB_NESBMODE_OFF
+    em.$reg16[$R.LRFD_BUFRAM_BASE + $R.PBE_GENERIC_RAM_O_MAXLEN] = 32 // TODO
+    em.$reg16[$R.LRFD_BUFRAM_BASE + $R.PBE_GENERIC_RAM_O_RXTIMEOUT] = timeout * 4
+    em.$reg16[$R.LRFD_BUFRAM_BASE + $R.PBE_GENERIC_RAM_O_FIRSTRXTIMEOUT] = timeout * 4
+    RfFreq.program(freqFromChan(chan))
+    $R.LRFDDBELL.IMASK0.$$ |= e$`LRF_EventOpDone` | e$`LRF_EventOpError`
+    IntrVec.NVIC_enable(e$`LRFD_IRQ0_IRQn`)
+    while (em.$reg32[$R.LRFD_BUFRAM_BASE + $R.PBE_COMMON_RAM_O_MSGBOX] == 0) { }
+    $R.SYSTIM.CH2CC.$$ = $R.SYSTIM.TIME250N.$$
+    $R.LRFDPBE.API.$$ = $R.PBE_GENERIC_REGDEF_API_OP_RX
+}
 
+
+export function LRFD_IRQ0_isr$$() {
+    const mis = $R.LRFDDBELL.MIS0.$$
+    $R.LRFDDBELL.ICLR0.$$ = mis
+    // em.@"%%[>]"(mis)
+    // em.@"%%[a]"()
+    if ((mis & e$`LRF_EventOpError`) != 0) {
+        $['%%>'](em.$reg16[$R.LRFD_BUFRAM_BASE + $R.PBE_COMMON_RAM_O_ENDCAUSE])
+        fail()
+    }
+    if ((mis & e$`LRF_EventSystim1`) != 0) {
+        rx_timeout = true
+    }
+    // if ((mis & $R.LRF_EventRxOk) != 0) {
+    //     em.print("peek {x}\n", .{RfFifo.peek(0)})
+    // }
+    IntrVec.NVIC_clear(e$`LRFD_IRQ0_IRQn`)
+    setState(State.READY)
+}
+
+export function em$run() {
+    enable()
+}
