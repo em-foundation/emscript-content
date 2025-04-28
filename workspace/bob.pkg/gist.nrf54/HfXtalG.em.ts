6b58449af785ba3ffb2d8e67446f83163177d414
bob frankel <bob@biosbob.biz>
2025-04-28 14:57:23 -0700

    HfXtal: WIP

--- workspace/bob.pkg/gist.nrf54/HfXtalG.em.ts
+++ workspace/bob.pkg/gist.nrf54/HfXtalG.em.ts
@@ -1,6 +1,8 @@
 import em from '@$$emscript'
 export const $U = em.$declare('MODULE')
 
+import * as $R from '@nordic.distro.nrf54/REGS.em'
+
 import * as Common from '@em.mcu/Common.em'
 import * as HfXtal from '@nordic.mcu.nrf54/HfXtal.em'
 
@@ -9,12 +11,14 @@ export namespace em$meta { }
 //>> ---- em$targ ---- <<//
 
 export function em$run() {
+    $['%%>']($R.RESET.RESETREAS.$$)
+    $R.RESET.RESETREAS.$$ = 0xFFFF
     Common.GlobalInterrupts.$$.enable()
     HfXtal.start()
     HfXtal.wait()
     HfXtal.stop()
-    Common.BusyWait.$$.wait(100000)
-    $['%%d']
+    Common.BusyWait.$$.wait(1000)
+    $['%%d:'](4)
     HfXtal.start()
     HfXtal.wait()
     HfXtal.stop()
