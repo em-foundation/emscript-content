f0de610b4979abbf7a6fcc14399646b71a80b048
bob frankel <bob@biosbob.biz>
2025-05-15 16:33:11 -0700

    adi.Mcu: SIMO setup

--- workspace/adi.max326xx/adi.mcu.max326xx/Mcu.em.ts
+++ workspace/adi.max326xx/adi.mcu.max326xx/Mcu.em.ts
@@ -10,6 +10,15 @@ import * as SysOsc from '@adi.mcu.max326xx/SysOsc.em'
 export namespace em$meta { }
 
 export function startup(): void {
+    const vrego_A = $R.F_SIMO_VREGO_A_RANGEA | ((1750 - 600) / 10)
+    $R.SIMO.VREGO_A.$$ = vrego_A
+    $R.SIMO.VREGO_A.$$ = vrego_A
+    const vrego_B = $R.F_SIMO_VREGO_B_RANGEB | ((1100 - 600) / 10)
+    $R.SIMO.VREGO_B.$$ = vrego_B
+    $R.SIMO.VREGO_B.$$ = vrego_B
+    const vrego_C = $R.F_SIMO_VREGO_C_RANGEC | ((1100 - 600) / 10)
+    $R.SIMO.VREGO_C.$$ = vrego_C
+    $R.SIMO.VREGO_C.$$ = vrego_C
     SysOsc.startup()
     $R.ICC0.CTRL.$$ |= $R.F_ICC_CTRL_EN
     $R.GCR.PCLKDIS0.$$ &= ~($R.F_GCR_PCLKDIS0_GPIO0 | $R.F_GCR_PCLKDIS0_GPIO1)
