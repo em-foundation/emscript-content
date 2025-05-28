2e401d2073fb92daaf085be438aaae57e20a5c26
bob frankel <bob@biosbob.biz>
2025-05-28 08:40:10 -0700

    $default: WIP

--- workspace/bob.pkg/bob.test/DefaultTest.em.ts
+++ workspace/bob.pkg/bob.test/DefaultTest.em.ts
@@ -1,18 +1,18 @@
 import em from '@$$emscript'
 export const $U = em.$declare('MODULE')
 
-class Tab extends $vector<u8> { $len = 3 }
-
-class S extends $struct {
-    x: u32
-    t: Tab
-}
+// class Tab extends $vector<u8> { $len = 3 }
 // 
-// const cfg = $config<bool_t>()
+// class S extends $struct {
+//     x: u32
+//     t: Tab
+// }
+// 
+const cfg = $config<u32>()
 
 export namespace em$meta {
     export function em$construct() {
-        console.log(S.$make())
+        console.log(cfg.$$)
     }
 }
 
