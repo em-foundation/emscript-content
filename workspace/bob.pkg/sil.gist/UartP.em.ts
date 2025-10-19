2a9ba1f727435be89da578d93bc8722fb8adf257
bob frankel <bob@biosbob.biz>
2025-10-19 16:31:15 -0700

    sil.mcu: ConsoleUart0

--- workspace/bob.pkg/sil.gist/UartP.em.ts
+++ workspace/bob.pkg/sil.gist/UartP.em.ts
@@ -9,7 +9,7 @@ const TxPin = $clone(GpioT)
 
 export namespace em$meta {
     export function em$configure() {
-        TxPin.pin_num.$$val = 0xB01
+        TxPin.pin_num.$$val = 0xB02
     }
 }
 
@@ -21,9 +21,12 @@ export function em$run() {
     $R.CMU.CLKEN0_SET.$$ = $R.CMU_CLKEN0_USART0
     $R.USART0.EN_SET.$$ = 1
     $R.GPIO.USARTROUTE[0].ROUTEEN.$$ = $R.GPIO_USART_ROUTEEN_TXPEN
-    $R.GPIO.USARTROUTE[0].TXROUTE.$$ = 0x10001
-    const div: u32 = 0x00004b68
+    $R.GPIO.USARTROUTE[0].TXROUTE.$$ = 0x40001
+    const div: u32 = 0x0000_0800
     $R.USART0.CLKDIV_SET.$$ = div
     $R.USART0.CMD_SET.$$ = $R.USART_CMD_TXEN
-    $R.USART0.TXDATA_SET.$$ = 0x55
+    for (const i of $range(10)) {
+        $R.USART0.TXDATA_SET.$$ = 0x55 + i
+        while (($R.USART0.STATUS.$$ & $R.USART_STATUS_TXC) == 0) { }
+    }
 }
--- workspace/silabs.efr32x/silabs.mcu.efr32x/ConsoleUart0.em.ts
+++ workspace/silabs.efr32x/silabs.mcu.efr32x/ConsoleUart0.em.ts
@@ -1,15 +1,34 @@
 import '@$$emscript'
 export const $U = $declare('MODULE', ConsoleUartI)
 
+import * as $R from '@silabs.distro.efr32x/REGS.em'
+
 import * as ConsoleUartI from '@em.hal/ConsoleUartI.em'
+import * as GpioI from '@em.hal/GpioI.em'
+
+export const TxPin = $proxy<GpioI.$I>()
 
 export namespace em$meta { }
 
 //>> ---- em$targ ---- <<//
 
+export function em$startup() {
+    TxPin.makeOutput()
+    TxPin.set()
+    $R.CMU.CLKEN0_SET.$$ = $R.CMU_CLKEN0_USART0
+    $R.USART0.EN_SET.$$ = 1
+    $R.GPIO.USARTROUTE[0].ROUTEEN.$$ = $R.GPIO_USART_ROUTEEN_TXPEN
+    $R.GPIO.USARTROUTE[0].TXROUTE.$$ = 0x10001
+    const div: u32 = 0x0000_0800
+    $R.USART0.CLKDIV_SET.$$ = div
+    $R.USART0.CMD_SET.$$ = $R.USART_CMD_TXEN
+}
+
 export function flush() {
+    while (($R.USART0.STATUS.$$ & $R.USART_STATUS_TXBL) == 0) { }
 }
 
 export function put(data: u8) {
+    $R.USART0.TXDATA_SET.$$ = data
     flush()
 }
