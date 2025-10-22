c9bcd7680cc5fa0be27068ddc9552a75368b1418
bob frankel <bob@biosbob.biz>
2025-10-21 18:15:10 -0700

    sil: Ex09, Ex10

--- workspace/silabs.efr32x/silabs.mcu.efr32x/OneShotTimer0.em.ts
+++ workspace/silabs.efr32x/silabs.mcu.efr32x/OneShotTimer0.em.ts
@@ -20,8 +20,11 @@ export namespace em$meta {
 var cur_arg: arg_t
 var cur_fxn: Handler = $null
 
+export function em$startup() {
+    $R.CMU.CLKEN0_SET.$$ = $R.CMU_CLKEN0_TIMER0
+}
+
 export function disable(): void {
-    // $R.TIMER0.CMD.$$ = $R.TIMER_CMD_STOP
     $R.TIMER0.EN_CLR.$$ = 1
     Idle.setPauseOnly(false)
     IntrVec.NVIC_disable(e$`TIMER0_IRQn`)
@@ -39,23 +42,17 @@ function ustart(usecs: u32, handler: OneShotI.Handler, arg: arg_t) {
     cur_fxn = handler
     cur_arg = arg
     Idle.setPauseOnly(true)
-    // $['%%>']($R.TIMER0.IEN.$$)
-    // printf`IEN = %08x\n`($R.TIMER0.IEN.$$)
-    $R.CMU.CLKEN0_SET.$$ = $R.CMU_CLKEN0_TIMER0
     $R.TIMER0.EN_CLR.$$ = 1
     const pre = 20 << $R._TIMER_CFG_PRESC_SHIFT
     $R.TIMER0.CFG_SET.$$ = pre | $R.TIMER_CFG_OSMEN | $R.TIMER_CFG_MODE_DOWN
     $R.TIMER0.EN_SET.$$ = 1
     $R.TIMER0.TOP.$$ = $R.TIMER0.CNT.$$ = usecs
-    IntrVec.NVIC_clear(e$`TIMER0_IRQn`)
-    $R.TIMER0.IF_CLR.$$ = $R.TIMER_IF_UF
     $R.TIMER0.IEN_SET.$$ = $R.TIMER_IEN_UF
     IntrVec.NVIC_enable(e$`TIMER0_IRQn`)
     $R.TIMER0.CMD.$$ = $R.TIMER_CMD_START
 }
 
 export function TIMER0_isr$$() {
-    $R.TIMER0.IEN_CLR.$$ = $R.TIMER_IEN_UF
     $R.TIMER0.IF_CLR.$$ = $R.TIMER_IF_UF
     const fxn = cur_fxn
     disable()
