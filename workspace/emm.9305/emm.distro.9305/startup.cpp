e5d7da836b966226d87af7ecf6d57dd3234337d3
bob frankel <bob@biosbob.biz>
2025-12-11 07:16:44 -0800

    persist data over sleep

--- workspace/emm.9305/emm.distro.9305/startup.cpp
+++ workspace/emm.9305/emm.distro.9305/startup.cpp
@@ -14,33 +14,36 @@ extern uint32_t __data_size__;
 extern uint32_t __global_pointer__;
 extern uint32_t __stack_top__;
 
+extern "C" bool isWarm_$$();
 extern "C" int main();
 
 extern "C" __attribute__ ((section(".entry"), noreturn)) void em__start() {
     asm ("mov_s	%sp,__stack_top__");
-    uint32_t *src;
-    uint32_t *dst;
-    uint32_t sz;
-    sz = (uint32_t)&__bss_size__;
-    dst = &__bss_addr__;
-    while (sz--) {
-        *dst++ = 0;
-    }
-    sz = (uint32_t)&__data_size__;
-    src = &__data_load__;
-    dst = &__data_addr__;
-    while (sz--) {
-        *dst++ = *src++;
-    }
+    if (!isWarm_$$()) {
+        uint32_t *src;
+        uint32_t *dst;
+        uint32_t sz;
+        sz = (uint32_t)&__bss_size__;
+        dst = &__bss_addr__;
+        while (sz--) {
+            *dst++ = 0;
+        }
+        sz = (uint32_t)&__data_size__;
+        src = &__data_load__;
+        dst = &__data_addr__;
+        while (sz--) {
+            *dst++ = *src++;
+        }
 #if __EM_BOOT_FLASH__ == 1
-    sz = (uint32_t)&__code_size__;
-    src = &__code_load__;
-    dst = &__code_addr__;
-    while (sz--) {
-        *dst++ = *src++;
+        sz = (uint32_t)&__code_size__;
+        src = &__code_load__;
+        dst = &__code_addr__;
+        while (sz--) {
+            *dst++ = *src++;
+        }
+        *em::$reg32((uint32_t)&SYS->RegMemCfg) |= MEM_DRAM6_IN_ICCM_MASK;
+ #endif
     }
-    *em::$reg32((uint32_t)&SYS->RegMemCfg) |= MEM_DRAM6_IN_ICCM_MASK;
-#endif
     main();
     __builtin_unreachable();    
 }
