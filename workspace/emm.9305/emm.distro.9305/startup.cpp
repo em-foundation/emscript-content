1364dd5d2ebbed4ef0dad297c8dd5be739c83901
bob frankel <bob@biosbob.biz>
2025-12-31 11:04:49 -0800

    sram wakeup working

--- workspace/emm.9305/emm.distro.9305/startup.cpp
+++ workspace/emm.9305/emm.distro.9305/startup.cpp
@@ -19,7 +19,6 @@ extern "C" int main();
 extern "C" void COMMON_WaitUs(uint32_t us);
 extern "C" void memcpy32(uint32_t *pDestination, const uint32_t *pSource, uint32_t numBytes);
 
-#define DBG 3
 
 extern "C" __attribute__ ((section(".entry"), noreturn)) void em__start() {
     asm ("mov_s	%sp,__stack_top__");
@@ -34,30 +33,21 @@ extern "C" __attribute__ ((section(".entry"), noreturn)) void em__start() {
             *dst++ = 0;
             sz -= 1;
         }
-        // volatile uint32_t flag = 1;
-        // while (flag) ;
         sz = (uint32_t)&__data_size__;
         src = &__data_load__;
         dst = &__data_addr__;
         memcpy32(dst, src, sz * 4);
-#if __EM_BOOT_FLASH__ == 1
+    #if __EM_BOOT_FLASH__ == 1
         sz = (uint32_t)&__code_size__;
         src = &__code_load__;
         dst = &__code_addr__;
         memcpy32(dst, src, sz * 4);
-        *em::$reg32((uint32_t)&SYS->RegMemCfg) |= MEM_DRAM5_IN_ICCM_MASK;
- #endif
+    #endif
     }
 
-    // GPIO->RegGPIOOutputEn.r32 |= 1 << DBG;
-    // GPIO->RegGPIODataOut.r32 ^= (1 << DBG);
-    // COMMON_WaitUs(1000);
-    // GPIO->RegGPIODataOut.r32 ^= (1 << DBG);
-    // COMMON_WaitUs(1000);
-    // GPIO->RegGPIODataOut.r32 ^= (1 << DBG);
-    // COMMON_WaitUs(1000);
-    // GPIO->RegGPIODataOut.r32 ^= (1 << DBG);
-    // COMMON_WaitUs(10000);
+    #if __EM_BOOT_FLASH__ == 1
+        *em::$reg32((uint32_t)&SYS->RegMemCfg) |= MEM_DRAM5_IN_ICCM_MASK;
+    #endif
 
     main();
     __builtin_unreachable();    
