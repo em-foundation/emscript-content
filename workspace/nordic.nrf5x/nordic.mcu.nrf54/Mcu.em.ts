e8334bc9c55bb0d095dc70d0cc3699b5bf22901d
bob frankel <bob@biosbob.biz>
2026-03-20 05:08:14 -0700

    54 MCU: mem fix

--- workspace/nordic.nrf5x/nordic.mcu.nrf54/Mcu.em.ts
+++ workspace/nordic.nrf5x/nordic.mcu.nrf54/Mcu.em.ts
@@ -43,9 +43,9 @@ export function startup(): void {
         $R.MEMCONF.POWER[0].RET.$$ = 0x3
         $R.MEMCONF.POWER[0].RET2.$$ = 0x3
     }
-    $R.MEMCONF.POWER[1].CONTROL.$$ = 0x0
-    $R.MEMCONF.POWER[1].RET.$$ = 0x0
-    $R.MEMCONF.POWER[1].RET2.$$ = 0x0
+    // $R.MEMCONF.POWER[1].CONTROL.$$ = 0x0
+    // $R.MEMCONF.POWER[1].RET.$$ = 0x0
+    // $R.MEMCONF.POWER[1].RET2.$$ = 0x0
     $R.CLOCK.LFCLK.SRC.$$ = $R.CLOCK_LFCLK_SRC_SRC_LFXO
     $R.CLOCK.TASKS_LFCLKSTART.$$ = 1
     Debug.startup()
